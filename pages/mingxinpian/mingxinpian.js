aab11cb37d61c2171fed617adc18ae046ee5c93a
wangqingfa <39437345+wangqingfa@users.noreply.github.com>
2019-05-23 17:13:15 +0800

    201905231713
    wqf

--- pages/mingxinpian/mingxinpian.js
+++ pages/mingxinpian/mingxinpian.js
@@ -5,14 +5,16 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    grids: [0, 1, 2, 3, 4, 5, 6, 7, 8]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+wx.setNavigationBarTitle({
+  title: '更多明信片',
+})
   },
 
   /**
