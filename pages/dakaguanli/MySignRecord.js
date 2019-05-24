b673e47f69ea64229c302f7811b9315e281fc016
LiuYang <Stablely@hotmail.com>
2019-05-24 11:30:32 +0800

    20190524

--- pages/dakaguanli/MySignRecord.js
+++ pages/dakaguanli/MySignRecord.js
@@ -1,13 +1,18 @@
-// pages/dakaguanli/MySignRecord.js
+var comm = require('../../utils/PublicProtocol.js'); //引用post公共函数
+var appid = wx.getStorageSync('appid'); //获取缓存中APPID
+var uuid = wx.getStorageSync('uuid'); //获取缓存中设备ID
+var utoken = wx.getStorageSync('utoken'); //获取缓存中用户utoken
+var pageindex = 1; //获取的页码
+var Flag = 0; //0刷新，1加载更多
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    isxs: false
+    isxs: false, //是否显示加载更多
+    isyw: true, //是否显示暂无数据
+    cardrecordlist: []
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
@@ -16,8 +21,45 @@ Page({
     wx.setNavigationBarTitle({
       title: '我的打卡记录'
     })
+    wx.hideLoading()
+    this.getlist(); 
   },
-
+  clickgd:function(){//加载更多
+    Flag = 1; //0刷新，1加载更多
+    this.getlist();
+  },
+  //获取数据列表
+  getlist: function() {
+    var tempData = {
+      uuid: uuid, //设备id
+      appid: appid, //
+      isboss: '0', //是否领导
+      pagesize: 10, //每页条数
+      pageindex: pageindex, //页码
+      utoken: utoken
+    }
+    var that = this;
+    comm.unitWebsitePro('PostCardRecordList', tempData, function(data) {
+      if (data.RspCode == "0000") { //正常 
+        if (Flag==0){
+          that.setData({
+            cardrecordlist: data.RspData.cardrecordlist, //绑定数据列表
+            isxs: true //显示加载更多
+          });
+        }else{
+          that.setData({//加载更多数组拼接
+            cardrecordlist: that.data.cardrecordlist.concat(data.RspData.cardrecordlist),  
+            isxs: true  
+          });
+        }
+        pageindex++;
+      } else { //暂无数据
+        that.setData({
+          isyw: false //显示暂无数据标签
+        });
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
